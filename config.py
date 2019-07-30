78c8136ed31d18dbcbc6ae006211d5f5e23a4872
RMIHRG <53496233+RMIHRG@users.noreply.github.com>
2019-07-30 17:38:27 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+username = 'pullthefuckingtriggr'
+password = 'pythonbot'
+client_id = 'y1Hj1dNcpdYHSg'
+client_secret = 'Ro9kXjSMse5Crr94FAhZI9aOcT8'
+
+
+#Subreddit
+subreddit = "rareinsults"
+
+#Number of posts to scan at a time (Suggested = 15)
+scanAmount = 8
+
+#How long it should wait between each scan in seconds (More posts should make this number way less, although, faster pc = handle more posts and scans than a slower one) (Suggested = 30)
+scanFrequency = 90
+
+#The threshold to delete posts at (Suggested = -4) (Testing = 0)
+threshold = -4
+
+#The threshold to delete the "I am a bot" comment to remove clutter (set to 0 if you dont want it to delete) (Suggested = 10) (Testing = 2)
+thresholdSelfDelete = 100
+
+#The message to comment on posts. If you dont want to to post this each time make the string empty (but leave the variable there)
+templateMessage = "Upvote this comment if you feel this submission is characteristic of our subreddit. Downvote this if you feel that it is not. If this comment's score falls below a certain number, this submission will be automatically removed."
+
+#The message to send once their comment gets deleted
+templateMailSubject = "Post was removed"
+template_mail_body = "Your [post]({url}) at /r/" + subreddit + ", was deleted because it reached below a threshold. If you think this was a mistake message us [here](https://www.reddit.com/message/compose?to=%2Fr%2F" + subreddit + "&subject=&message=)"
---
+++ reddit_bot.py
@@ -0,0 +1,130 @@
+import praw
+import config
+import time
+import os
+from random import *
+
+
+
+def authentication():
+	print ("Authenticating...")
+	reddit = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "bot for rareinsults")
+	print ("Authenticated as {}".format(reddit.user.me()))
+	time.sleep(2)
+	return reddit
+
+
+def run_bot(reddit, posts_replied_to, exclude_comments, safe_posts):
+	time.sleep(1)
+	print ("Scanning posts\n==============")
+	me = reddit.user.me()
+	for contributor in reddit.subreddit(config.subreddit).contributor():
+		if contributor not in contributors:
+			print ("Found new approved submitter, {}. Adding to list.".format(contributor))
+			contributors.append(contributor)
+			with open ("contributors.txt", "a") as s:
+				s.write(str(contributor) + "\n")
+	for post in reddit.subreddit(config.subreddit).new(limit=config.scanAmount):
+		if post.id not in list(posts_replied_to) and config.templateMessage and not None and post.author not in contributors:
+			print ("Commenting on: \"" + post.title + "\" \"" + post.id + "\" by: \"" + post.author.name + "\"\n")
+			post.reply(config.templateMessage).mod.distinguish(sticky=True)
+			posts_replied_to.append(post.id)
+			with open ("posts_replied_to.txt", "a") as f:
+				f.write(post.id + "\n")
+
+	for comment in me.comments.new(limit=config.scanAmount + 50):
+		if comment.banned_by is not None:
+			print (comment.banned_by + " removed my comment on post \"" + comment.submission.title + "\". Adding to whitelist.")
+			comment.delete()
+		elif comment.id not in list(safe_posts) and comment.id not in list(exclude_comments) and comment.score <= config.threshold:
+			print ("Post \"" + comment.submission.title + "\" reached delete threshold. Deleting...")
+			exclude_comments.append(comment.id)
+			with open ("exclude_comments.txt", "a") as s:
+				s.write(comment.id + "\n")
+			mail_body = config.template_mail_body.format(url=comment.submission.url)
+			message1 = comment.submission.permalink
+			comment1 = comment.submission.reply("This submission has been removed because the stickied bot comment reached the downvote threshold. Please contact the moderation team if you have questions regarding this removal.")
+			comment1.mod.distinguish(sticky=True)
+			comment.submission.mod.flair('Removed', 'Removed')
+			comment.report("ERROR #69, FOR ITEM (comment.submission) DWVT THRESHOLD HAS BEEN REACHED. NOTIFY WHOM IT MAY CONCERN")
+			comment.submission.mod.lock()
+			comment.submission.mod.remove()
+			print('removed')
+		elif comment.score >= config.thresholdSelfDelete and config.thresholdSelfDelete != 0 and comment.submission.id not in list(safe_posts):
+			print ("Post \"" + comment.submission.title + "\" reached SAFE threshold. Editing bot comment...")
+			editedbody = "i wish i was as [COOL](https://imgur.com/YHSkXya) and [RAD](https://imgur.com/4UdnD5X) as OP"
+			comment.edit(editedbody)
+			safe_posts.append(comment.submission.id)
+			with open ("safe_posts.txt", "a") as s:
+				s.write(comment.submission.id + "\n")
+		elif comment.id not in list(exclude_comments):
+				print ( "\"" + comment.submission.title + "\" - " + str(comment.score))
+
+
+	print ("==============\n\nSleeping for " + str(config.scanFrequency) + " seconds...\n")
+	time.sleep(config.scanFrequency)
+
+def get_saved_posts(): #Posts that the bot has already commented on and doesnt need to comment on again. This will be a very populated file as it doesnt delete anything just for the sake of not spamming posts
+	if not os.path.isfile("posts_replied_to.txt"): #Only thing that uses os so that you dont have to create the file.
+		posts_replied_to = []
+	else:
+		with open("posts_replied_to.txt", "r") as f:
+			posts_replied_to = f.read()
+			posts_replied_to = posts_replied_to.split("\n")
+			posts_replied_to = filter(None, posts_replied_to)
+
+	return posts_replied_to
+
+
+def exclude_comments(): #This is for posts that have already been removed. Called comments because we dont want the bot watching its own comments if its already been deleted.
+	if not os.path.isfile("exclude_comments.txt"): #Only thing that uses os so that you dont have to create the file.
+		exclude_comments = []
+	else:
+		with open("exclude_comments.txt", "r") as f:
+			exclude_comments = f.read()
+			exclude_comments = exclude_comments.split("\n")
+			exclude_comments = filter(None, exclude_comments)
+
+	return exclude_comments
+
+
+def safe_posts(): #This is for post that have been "accepted" by the subreddit. refer to "thresholdSelfDelete" in config.py
+	if not os.path.isfile("safe_posts.txt"): #Only thing that uses os so that you dont have to create the file.
+		safe_posts = []
+	else:
+		with open("safe_posts.txt", "r") as f:
+			safe_posts = f.read()
+			safe_posts = safe_posts.split("\n")
+			safe_posts = filter(None, safe_posts)
+
+	return safe_posts
+
+def contributors():
+	if not os.path.isfile("contributors.txt"):
+		contributors = []
+	else:
+		with open("contributors.txt", "r") as f:
+			contributors = f.read()
+			contributors = contributors.split("\n")
+			contributors = filter(None, contributors)
+
+	return contributors
+
+
+#Boot Info
+
+reddit = authentication()
+safe_posts = list(safe_posts())
+print ("Safe Posts: " + str(safe_posts))
+exclude_comments = list(exclude_comments())
+print ("Excluded Comments: " + str(exclude_comments))
+posts_replied_to = list(get_saved_posts())
+print ("Commented Posts: " + str(posts_replied_to) + "\n")
+contributors = list(contributors())
+
+while True:
+	run_bot(reddit, posts_replied_to, exclude_comments, safe_posts)
